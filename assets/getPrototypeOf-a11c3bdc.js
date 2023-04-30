a48f018f97d35d8709c34b2f38fee8f9b08745f5
LeoCantThinkOfAName <LeoCantThinkOfAName@users.noreply.github.com>
2023-04-30 03:47:00 +0000

    deploy: fff085f839bd91e2b92713202ad754c3046f9c71

---
+++ assets/getPrototypeOf-a11c3bdc.js
@@ -0,0 +1,2 @@
+function o(t){if(t===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return t}function r(t){return r=Object.setPrototypeOf?Object.getPrototypeOf.bind():function(e){return e.__proto__||Object.getPrototypeOf(e)},r(t)}export{o as _,r as a};
+//# sourceMappingURL=getPrototypeOf-a11c3bdc.js.map

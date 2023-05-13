e3b7aa5f7f7b07389d636145d1d950b0560e2cc8
LeoCantThinkOfAName <LeoCantThinkOfAName@users.noreply.github.com>
2023-05-13 09:05:59 +0000

    deploy: 3d4eba157f1d7a78066c64015b7d0d5412bcd29c

---
+++ assets/PostFooter.stories-ed64672e.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-94f6e698.js";import{P as a}from"./PostFooter-1345423e.js";import{C as p}from"./chunk-S432VF2S-c48cc12a.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index.esm-0e9bfc8d.js";import"./chunk-NAA7TEES-6c8d5cf8.js";import"./chunk-QEVFQ4EU-314b74ab.js";import"./extends-bab83b91.js";import"./chunk-NO6MRLPK-1e12c6d9.js";import"./emotion-react.browser.esm-69ad734b.js";import"./index-bf97d06a.js";import"./useTranslation-97f43e0c.js";import"./nonIterableRest-6506dcc9.js";import"./createClass-d459cbf5.js";import"./context-27f4b450.js";import"./chunk-IJBC3YLI-3546d64b.js";import"./chunk-MPFPK3CX-863f2a69.js";import"./chunk-IXKZFKRY-6a76013f.js";import"./chunk-DKFDJSXF-7e47dccf.js";import"./chunk-3ZTTLJBV-4f228e71.js";const R={title:"Post/Post Footer",component:a,tags:["autodocs"],argTypes:{},decorators:[s=>r.jsx(p,{children:r.jsx(s,{})})]},o={args:{tags:["Hello","World"]},name:"Default"};var t,m,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    tags: ["Hello", "World"]
+  },
+  name: "Default"
+}`,...(e=(m=o.parameters)==null?void 0:m.docs)==null?void 0:e.source}}};const S=["Default"];export{o as Default,S as __namedExportsOrder,R as default};
+//# sourceMappingURL=PostFooter.stories-ed64672e.js.map

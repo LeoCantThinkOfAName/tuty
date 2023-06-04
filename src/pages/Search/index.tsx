69980e21f08f7d6c880adc5988fa410c2e10c87e
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-06-04 23:31:25 +0800

    feat: replace with useInfiniteQuery

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@chakra-ui/react";
 import { FC } from "react";
 import { Post } from "../../components/Post";
 import { useParams } from "react-router-dom";
@@ -5,14 +6,17 @@ import { usePosts } from "../../services";
 
 export const SearchPage: FC = () => {
   const { term } = useParams();
-  const { data } = usePosts();
+  const { data, fetchNextPage } = usePosts(term ?? "");
 
   if (!data) return null;
   return (
     <>
-      {data.map((post) => (
-        <Post key={post.id} data={post} />
-      ))}
+      {data.pages.map((page) =>
+        page?.data.map((pageData) => (
+          <Post key={pageData.id} data={pageData} />
+        )),
+      )}
+      <Button onClick={() => fetchNextPage()}>click</Button>
     </>
   );
 };
--- src/services/usePosts.ts
+++ src/services/usePosts.ts
@@ -1,21 +1,60 @@
-import { supabase } from "./clients";
-import { useQuery } from "@tanstack/react-query";
+import { QueryFunctionContext, useInfiniteQuery } from "@tanstack/react-query";
 
-const fetchPosts = async () => {
+import { supabase } from "./clients";
+
+const queryFn = async (pageParam: number = 1, term: string) => {
   try {
-    const { data, error } = await supabase
+    const query = supabase
       .from("posts")
       .select(
         "*, author:profiles (id, name, img, title), category:categories (*)",
       )
       .order("createdAt", { ascending: false });
-    if (data) return data;
+
+    if (!!term) query.textSearch("posts_fulltext", term);
+
+    const from = pageParam * 10;
+    const to = (pageParam + 1) * 10;
+    const { data, error } = await query.range(from, to);
+    if (data) return { data: data, nextPage: pageParam + 1 };
     if (error) throw error;
   } catch (error) {
     console.error(error);
   }
 };
 
-export const usePosts = () => {
-  return useQuery(["posts"], fetchPosts);
+const getNextPageParam = (lastPage: Awaited<ReturnType<typeof queryFn>>) => {
+  if (lastPage?.data.length !== 10) return undefined;
+  else return lastPage.nextPage;
+};
+
+export const usePosts = (term: string) => {
+  return useInfiniteQuery({
+    queryKey: ["posts", { term }],
+    queryFn: async ({
+      pageParam,
+      queryKey,
+    }: QueryFunctionContext<[string, { term: string }], number>) => {
+      console.log(pageParam);
+      const term = queryKey[1].term;
+      try {
+        const query = supabase
+          .from("posts")
+          .select(
+            "*, author:profiles (id, name, img, title), category:categories (*)",
+          )
+          .order("createdAt", { ascending: false });
+
+        if (!!term) query.textSearch("posts_fulltext", term);
+
+        const from = (pageParam ? pageParam * 10 : 0) + 1;
+        const { data, error } = await query.range(from, from + 9);
+        if (data) return { data: data, nextPage: (pageParam || 0) + 1 };
+        if (error) throw error;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getNextPageParam,
+  });
 };

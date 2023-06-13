67fb3b0a7fe9f8408ef0ce2f6304540908aa21a1
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-06-13 20:59:23 +0800

    fix: #46 sticky header and sidebar issue

--- src/pages/CreatePost/useSubmit.ts
+++ src/pages/CreatePost/useSubmit.ts
@@ -4,6 +4,7 @@ import { FormEvent, useCallback } from "react";
 export const useSubmit = <T extends FieldValues>(methods: UseFormReturn<T>) => {
   return useCallback(
     (event: FormEvent<HTMLFormElement>) => {
+      console.log("event: ", event);
       methods.handleSubmit((data) => {
         console.log(data);
       })(event);

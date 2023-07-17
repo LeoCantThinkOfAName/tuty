3c8e45da17c66eb20ed81435cdde5936863e83e8
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-07-17 18:25:14 +0800

    feat: populate mock data for post component story

--- src/components/Post/stories/Post.stories.tsx
+++ src/components/Post/stories/Post.stories.tsx
@@ -15,6 +15,32 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
-  args: {},
+  args: {
+    data: {
+      authorId: "123",
+      content: "This is a post from Author",
+      categoryId: 1,
+      createdAt: new Date().toDateString(),
+      deletedAt: null,
+      updatedAt: null,
+      id: "123",
+      location: "Taipei",
+      objective: "Learn somthing new",
+      rate: 1000,
+      rateType: "day",
+      tags: ["Tag 1", "Tag 2"],
+      subject: "123",
+      author: {
+        id: "123",
+        img: "",
+        name: "Author",
+        title: "Job Title",
+      },
+      category: {
+        id: 1,
+        name: "Category",
+      },
+    },
+  },
   name: "Default",
 };

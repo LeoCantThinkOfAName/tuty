11c9e197822d80337cf406a09b0abb83c787bf57
LeoCantThinkOfAName <LeoCantThinkOfAName@users.noreply.github.com>
2023-07-17 06:51:25 +0000

    deploy: 925b59f476536aec99264e863787eed55529972f

---
+++ assets/SideBar.stories-8d116e5b.js
@@ -0,0 +1,37 @@
+import{j as r}from"./jsx-runtime-94f6e698.js";import{S as n}from"./index-b7251926.js";import{S as a}from"./StoryLayout-8b1ad32b.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-ffd16f0d.js";import"./index-dbb44d13.js";import"./useTranslation-e5e4d349.js";import"./context-f527a539.js";import"./i18nInstance-1f8a4d28.js";import"./chunk-YTV6DHKL-94ebb05f.js";import"./chunk-QEVFQ4EU-04d6573c.js";import"./extends-bab83b91.js";import"./index-bf97d06a.js";import"./index-647bcbfd.js";import"./index-d68b7e21.js";import"./chunk-DKFDJSXF-0f12136d.js";import"./chunk-LB6CWFOC-afb3a818.js";import"./index-84432276.js";import"./motion-bad323f9.js";import"./chunk-MPFPK3CX-341f25db.js";import"./chunk-GYFRIY2Z-39d2d048.js";import"./chunk-IJBC3YLI-af4fa993.js";import"./chunk-P74GIWPW-b554f208.js";import"./chunk-R3DH46PF-f0689d26.js";const v={title:"SideBar/SideBar",component:n,tags:["autodocs"],argTypes:{},parameters:{layout:"full"}},t={render:s=>r.jsx(a,{children:r.jsx(n,{...s})}),args:{menu:[{category:"First",items:[{title:"First child",path:"/"},{title:"Second child",path:"/first-second"}]},{category:"Second",items:[{title:"First child",path:"/second-first"},{title:"Second child",path:"/second-second"},{title:"Third child",path:"/second-third"}]},{category:"Third",items:[{title:"First child",path:"/third-first"}]}]},name:"Default"};var i,e,o;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <StoryLayout>
+      <SideBar {...props} />
+    </StoryLayout>,
+  args: {
+    menu: [{
+      category: "First",
+      items: [{
+        title: "First child",
+        path: "/"
+      }, {
+        title: "Second child",
+        path: "/first-second"
+      }]
+    }, {
+      category: "Second",
+      items: [{
+        title: "First child",
+        path: "/second-first"
+      }, {
+        title: "Second child",
+        path: "/second-second"
+      }, {
+        title: "Third child",
+        path: "/second-third"
+      }]
+    }, {
+      category: "Third",
+      items: [{
+        title: "First child",
+        path: "/third-first"
+      }]
+    }]
+  },
+  name: "Default"
+}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const w=["Default"];export{t as Default,w as __namedExportsOrder,v as default};
+//# sourceMappingURL=SideBar.stories-8d116e5b.js.map
--- assets/index-b84209fb.js
+++
@@ -1,2 +0,0 @@
-const e={ROOT:"/",SEARCH:"/search/:term",CREATE_POST:"/create-post"},t=[16,20];export{t as H,e as P};
-//# sourceMappingURL=index-b84209fb.js.map

5ee9ac120af1237dba47999f01cd0a32208af1c7
LeoCantThinkOfAName <LeoCantThinkOfAName@users.noreply.github.com>
2023-08-06 04:23:54 +0000

    deploy: 138205a170c9990fb3646bbc21ccbaa472085a9b

---
+++ assets/InputField.stories-8568983b.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-ffb262ed.js";import{I as a}from"./InputField-700eaa98.js";import{u as p,F as i}from"./index.esm-7066814d.js";import{S as u}from"./StoryLayout-d29be8a9.js";import{I as d}from"./chunk-6CVSDS6C-aad5dd30.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./chunk-XRMX4GAI-2b5e1d40.js";import"./chunk-ZJJGQIVY-06d8bb4d.js";import"./index-e1c9bcfd.js";import"./extends-15b115a7.js";import"./index-17e33fe9.js";import"./chunk-7COXQURZ-cb5d7755.js";import"./chunk-KRPLQIP4-28c7ffff.js";const P={title:"Input/Input Field",component:a,tags:["autodocs"],argTypes:{},parameters:{layout:"full"}},r={render:n=>{const s=p();return o.jsx(u,{children:o.jsx(i,{...s,children:o.jsx(d,{...n})})})},args:{name:"Some input"},name:"Default"};var t,e,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: props => {
+    const methods = useForm();
+    return <StoryLayout>
+        <FormProvider {...methods}>
+          <Input {...props} />
+        </FormProvider>
+      </StoryLayout>;
+  },
+  args: {
+    name: "Some input"
+  },
+  name: "Default"
+}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,P as default};
+//# sourceMappingURL=InputField.stories-8568983b.js.map

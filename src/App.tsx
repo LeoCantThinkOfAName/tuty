1566facfdda2baad91363ec40fbf515a14aedada
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-08-15 20:23:34 +0800

    feat: apply auth context, set global toast config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./constexts/AuthContext";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
@@ -8,8 +9,20 @@ import { theme } from "./styles/theme";
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme} resetCSS>
-        <RouterProvider router={routes}></RouterProvider>
+      <ChakraProvider
+        theme={theme}
+        resetCSS
+        toastOptions={{
+          defaultOptions: {
+            position: "bottom",
+            duration: 5000,
+            isClosable: true,
+          },
+        }}
+      >
+        <AuthProvider>
+          <RouterProvider router={routes}></RouterProvider>
+        </AuthProvider>
       </ChakraProvider>
     </QueryClientProvider>
   );

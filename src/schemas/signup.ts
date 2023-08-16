cdb69a888b2d8e4ed4f6a2d59cd4f35a216ea177
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-08-16 19:49:33 +0800

    feat: create signup form schema

---
+++ src/schemas/signup.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const signupSchema = z
+  .object({
+    email: z.string().email("invalidEmail").min(1, "required"),
+    password: z
+      .string()
+      .min(1, "required")
+      .min(8, "passwordTooShort")
+      .max(20, "passwordTooLong")
+      .refine((value) => /^[A-Z]+$/.test(value), "needUppercase")
+      .refine((value) => /^[a-z]+$/.test(value), "needLowercase")
+      .refine((value) => /^[0-9]+$/.test(value), "needNumber")
+      .refine((value) => /^[!@#$%^&*]+$/.test(value)),
+    passwordValidation: z.string().min(1, "required"),
+  })
+  .refine((data) => data.password === data.passwordValidation, {
+    message: "passwordsDontMatch",
+    path: ["passwordValidation"],
+  });
+
+export type SignupForm = z.infer<typeof signupSchema>;

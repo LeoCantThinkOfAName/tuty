d95e7c05fb6fe2cf97f86e309c35cd777bcb3e95
LeoCantThinkOfAName <LeoCantThinkOfAName@users.noreply.github.com>
2023-08-20 09:06:29 +0000

    deploy: b4f8b5ac75ea4cac2ac2389ccdd39dac29ccd1a0

---
+++ assets/Post.stories-bc4eac93.js
@@ -0,0 +1,36 @@
+import{a as p}from"./index-ad18f342.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-8cecad58.js";import"./useTranslation-ca5fece7.js";import"./context-90c9d743.js";import"./i18nInstance-1f8a4d28.js";import"./chunk-2OOHT3W5-91a4dd5c.js";import"./chunk-ZJJGQIVY-b66b1d78.js";import"./index-39ab5497.js";import"./extends-15b115a7.js";import"./chunk-PULVB27S-b3d725d2.js";import"./chunk-RPO2WXNL-100d6a52.js";import"./chunk-UBPTU7WB-be418a0b.js";import"./emotion-react.browser.esm-88c8e6c3.js";import"./chunk-2GBDXOMA-ca05fc42.js";import"./chunk-UVUR7MCU-d5c4f206.js";import"./chunk-5PH6ULNP-79b6aba4.js";import"./index-17e33fe9.js";import"./PostFooter-a86b76db.js";import"./index.esm-0ba758de.js";import"./index.esm-7066814d.js";import"./chunk-KRPLQIP4-32b7a1c6.js";import"./chunk-4IH3O7BJ-b9e2310f.js";import"./chunk-XRMX4GAI-9457f408.js";import"./PostHeader-147b6686.js";import"./typeof-7fd5df1e.js";import"./index-3e7aa64e.js";const t={authorId:"123",content:"This is a post from Author",categoryId:1,createdAt:new Date().toDateString(),deletedAt:null,updatedAt:null,id:"123",location:"Taipei",objective:"Learn somthing new",rate:1e3,rateType:"DAY",currencyId:1,currency:{id:1,name:"USD"},tags:["Tag 1","Tag 2"],subject:"123",author:{id:"123",img:"",name:"Author",title:"Job Title"},category:{id:1,name:"Category"}},q={title:"Post/Post",component:p,tags:["autodocs"],argTypes:{}},e={args:{data:t},name:"Default"};var r,o,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  authorId: "123",
+  content: "This is a post from Author",
+  categoryId: 1,
+  createdAt: new Date().toDateString(),
+  deletedAt: null,
+  updatedAt: null,
+  id: "123",
+  location: "Taipei",
+  objective: "Learn somthing new",
+  rate: 1000,
+  rateType: "DAY",
+  currencyId: 1,
+  currency: {
+    id: 1,
+    name: "USD"
+  },
+  tags: ["Tag 1", "Tag 2"],
+  subject: "123",
+  author: {
+    id: "123",
+    img: "",
+    name: "Author",
+    title: "Job Title"
+  },
+  category: {
+    id: 1,
+    name: "Category"
+  }
+}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var n,i,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    data
+  },
+  name: "Default"
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const z=["data","Default"];export{e as Default,z as __namedExportsOrder,t as data,q as default};
+//# sourceMappingURL=Post.stories-bc4eac93.js.map
---
+++ assets/index.stories-07bcd256.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{C as p}from"./index-d2b28a22.js";import{S as i}from"./StoryLayout-8a0449d3.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index.esm-7066814d.js";import"./InputField-54de1372.js";import"./chunk-XRMX4GAI-9457f408.js";import"./chunk-ZJJGQIVY-b66b1d78.js";import"./index-39ab5497.js";import"./extends-15b115a7.js";import"./index-17e33fe9.js";import"./chunk-6CVSDS6C-8bee5339.js";import"./defineProperty-4067c753.js";import"./toPropertyKey-377bf628.js";import"./typeof-7fd5df1e.js";import"./createClass-67c7d8cf.js";import"./setPrototypeOf-0bb37fbe.js";import"./floating-ui.dom-30bf267c.js";import"./emotion-react.browser.esm-88c8e6c3.js";import"./index-add592e3.js";import"./chunk-4IH3O7BJ-b9e2310f.js";import"./useTranslation-ca5fece7.js";import"./context-90c9d743.js";import"./i18nInstance-1f8a4d28.js";import"./chunk-2GBDXOMA-ca05fc42.js";import"./chunk-UVUR7MCU-d5c4f206.js";import"./chunk-5PH6ULNP-79b6aba4.js";import"./index-8cecad58.js";import"./chunk-KRPLQIP4-32b7a1c6.js";const B={title:"Pages/Create Post Page",component:p,tags:["autodocs"],argTypes:{},parameters:{layout:"full"}},t={render:a=>r.jsx(i,{children:r.jsx(p,{...a})}),args:{children:r.jsx(r.Fragment,{children:"test"})},name:"Default"};var o,e,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: props => <StoryLayout>
+      <CreatePostPage {...props} />
+    </StoryLayout>,
+  args: {
+    children: <>test</>
+  },
+  name: "Default"
+}`,...(m=(e=t.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const G=["Default"];export{t as Default,G as __namedExportsOrder,B as default};
+//# sourceMappingURL=index.stories-07bcd256.js.map

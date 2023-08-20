6138e9f70aba8483fb74fa59ac8a74ec8e640fd2
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-08-20 16:09:11 +0800

    feat: dynamic show menu item, toggle auth modal

--- src/components/Header/UserMenu.tsx
+++ src/components/Header/UserMenu.tsx
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  Button,
   Flex,
   Menu,
   MenuButton,
@@ -11,42 +12,48 @@ import {
 
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../../contexts/AuthContext";
 import { useTranslation } from "react-i18next";
 
 interface UserMenuProps {}
 
 export const UserMenu: FC<UserMenuProps> = () => {
+  const { session, toggle } = useAuthContext();
   const { t } = useTranslation();
 
-  return (
-    <Menu>
-      <MenuButton type="button">
-        <Avatar size="sm" />
-      </MenuButton>
-      <MenuList>
-        <Flex alignItems="center" justifyContent="space-between" px="5">
-          <Flex flexDir="column">
-            <Text fontWeight="bold">User Name</Text>
-            <Text color="gray.500">User Email</Text>
+  if (session) {
+    return (
+      <Menu>
+        <MenuButton type="button">
+          <Avatar size="sm" />
+        </MenuButton>
+        <MenuList>
+          <Flex alignItems="center" justifyContent="space-between" px="5">
+            <Flex flexDir="column">
+              <Text fontWeight="bold">User Name</Text>
+              <Text color="gray.500">User Email</Text>
+            </Flex>
+            <Avatar size="lg" />
           </Flex>
-          <Avatar size="lg" />
-        </Flex>
-        <MenuDivider />
+          <MenuDivider />
 
-        <MenuItem as={Link} to="/">
-          {t("userMenu.myAccount")}
-        </MenuItem>
+          <MenuItem as={Link} to="/">
+            {t("userMenu.myAccount")}
+          </MenuItem>
 
-        <MenuItem as={Link} to="/">
-          {t("userMenu.myPosts")}
-        </MenuItem>
+          <MenuItem as={Link} to="/">
+            {t("userMenu.myPosts")}
+          </MenuItem>
 
-        <MenuItem as={Link} to="/">
-          {t("userMenu.bookmarked")}
-        </MenuItem>
+          <MenuItem as={Link} to="/">
+            {t("userMenu.bookmarked")}
+          </MenuItem>
 
-        <MenuItem>{t("userMenu.logout")}</MenuItem>
-      </MenuList>
-    </Menu>
-  );
+          <MenuItem>{t("userMenu.logout")}</MenuItem>
+        </MenuList>
+      </Menu>
+    );
+  } else {
+    return <Avatar size="sm" p="0" minW="unset" as={Button} onClick={toggle} />;
+  }
 };
--- src/components/Header/tests/UserMenu.test.tsx
+++ src/components/Header/tests/UserMenu.test.tsx
@@ -1,9 +1,19 @@
+import * as Context from "../../../contexts/AuthContext";
+
 import { act, cleanup, render, screen, waitFor } from "@testing-library/react";
 import { afterEach, describe, expect, test, vi } from "vitest";
 
+import { Session } from "@supabase/supabase-js";
 import { UserMenu } from "../UserMenu";
 import { createWrapper } from "../../../utils/createWrapper";
 import userEvent from "@testing-library/user-event";
+
+const mockContext = (session: Session | null) => {
+  vi.spyOn(Context, "useAuthContext").mockImplementation(() => ({
+    toggle: () => {},
+    session,
+  }));
+};
 
 describe("UserMenu", () => {
   afterEach(() => {
@@ -18,13 +28,23 @@ describe("UserMenu", () => {
     expect(screen.getByRole("button")).toBeTruthy();
   });
 
-  test("Should able to open user menu", async () => {
+  test("Should able to open AuthModal", async () => {
+    mockContext(null);
+    render(<UserMenu />, {
+      wrapper: createWrapper(),
+    });
+    await act(async () => userEvent.click(screen.getByRole("button")));
+    await waitFor(() => expect(screen.findByRole("dialog")).toBeTruthy());
+  });
+
+  test("Should able have 4 list items", async () => {
+    mockContext({} as Session);
     render(<UserMenu />, {
       wrapper: createWrapper(),
     });
     await act(async () => userEvent.click(screen.getByRole("button")));
     await waitFor(() =>
-      expect(screen.queryAllByRole("menuitem")).toHaveLength(4),
+      expect(screen.queryAllByRole("menuitem")).toHaveLength(4)
     );
   });
 });
